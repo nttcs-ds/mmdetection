8c56d445ff6f3da386b9bfe9d4c5789082dea8c2
Jiro Nishitoba <jiro.nishitoba@retrieva.jp>
2021-10-20 20:38:33 +0900

    support multiple images inputs.

--- demo/image_demo.py
+++ demo/image_demo.py
@@ -1,24 +1,64 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-import asyncio
 from argparse import ArgumentParser
+import numpy
+from os.path import basename
+from os import chmod, makedirs
+from tqdm import tqdm
 
-from mmdet.apis import (async_inference_detector, inference_detector,
-                        init_detector, show_result_pyplot)
+from mmdet.apis import inference_detector, init_detector
+
+
+def show_result(model,
+                img,
+                result,
+                out,
+                score_thr=0.3,
+                title='result',
+                wait_time=0):
+    """Visualize the detection results on the image.
+    Args:
+        model (nn.Module): The loaded detector.
+        img (str or np.ndarray): Image filename or loaded image.
+        result (tuple[list] or list): The detection result, can be either
+            (bbox, segm) or just bbox.
+        score_thr (float): The threshold to visualize the bboxes and masks.
+        title (str): Title of the pyplot figure.
+        wait_time (float): Value of waitKey param.
+                Default: 0.
+    """
+    if hasattr(model, 'module'):
+        model = model.module
+    model.show_result(
+        img,
+        result,
+        score_thr=score_thr,
+        show=True,
+        wait_time=wait_time,
+        win_name=title,
+        bbox_color=(72, 101, 241),
+        text_color=(72, 101, 241),
+        out_file=out)
+
+
+def create_batch(images, batch_size):
+    chunked_list = []
+    for i in range(0, len(images), batch_size):
+        chunked_list.append(images[i: i + batch_size])
+    return chunked_list
 
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument('img', help='Image file')
     parser.add_argument('config', help='Config file')
     parser.add_argument('checkpoint', help='Checkpoint file')
+    parser.add_argument('img_output_dir', help='Output directory')
+    parser.add_argument('params_output_dir', help='Output directory')
+    parser.add_argument('img', nargs="+", help='Image file')
+    parser.add_argument('--batch', type=int, default=16, help='batch size')
     parser.add_argument(
         '--device', default='cuda:0', help='Device used for inference')
     parser.add_argument(
         '--score-thr', type=float, default=0.3, help='bbox score threshold')
-    parser.add_argument(
-        '--async-test',
-        action='store_true',
-        help='whether to set async options for async inference.')
     args = parser.parse_args()
     return args
 
@@ -26,25 +66,36 @@ def parse_args():
 def main(args):
     # build the model from a config file and a checkpoint file
     model = init_detector(args.config, args.checkpoint, device=args.device)
-    # test a single image
-    result, _ = inference_detector(model, args.img)
-    # show the results
-    show_result_pyplot(model, args.img, result, score_thr=args.score_thr)
-
-
-async def async_main(args):
-    # build the model from a config file and a checkpoint file
-    model = init_detector(args.config, args.checkpoint, device=args.device)
-    # test a single image
-    tasks = asyncio.create_task(async_inference_detector(model, args.img))
-    result = await asyncio.gather(tasks)
-    # show the results
-    show_result_pyplot(model, args.img, result[0], score_thr=args.score_thr)
+    img_data = create_batch(args.img, args.batch)
+    states_result = []
+    labels_result = []
+    bboxes_result = []
+    filenames = []
+    for d in tqdm(img_data):
+        result, intermediate = inference_detector(model, d)
+        states, labels, bboxes = intermediate
+        filenames.extend(d)
+        states_result.append(states.to('cpu').detach().numpy().copy())
+        labels_result.append(labels.to('cpu').detach().numpy().copy())
+        bboxes_result.append(bboxes.to('cpu').detach().numpy().copy())
+        for f, r in zip(d, result):
+            out_file = args.img_output_dir + basename(f)
+            show_result(model, f, r,
+                        score_thr=args.score_thr, out=out_file)
+    result_dir = args.params_output_dir
+    makedirs(result_dir, True)
+    chmod(result_dir, 0o755)
+    with open(result_dir + "states.npz", "wb") as f:
+        numpy.savez(f, numpy.vstack(states_result))
+    with open(result_dir + "labels.npz", "wb") as f:
+        numpy.savez(f, numpy.vstack(labels_result))
+    with open(result_dir + "bboxes.npz", "wb") as f:
+        numpy.savez(f, numpy.vstack(bboxes_result))
+    with open(result_dir + "filenames.txt", "w") as f:
+        for fname in filenames:
+            f.write(basename(fname) + "\n")
 
 
 if __name__ == '__main__':
     args = parse_args()
-    if args.async_test:
-        asyncio.run(async_main(args))
-    else:
-        main(args)
+    main(args)
